349b33218d9f53d015566dc25e2df8e74ac04296
victorhong1988 <victorhong1988@gmail.com>
2017-12-18 14:40:53 -0500

    Created jellyfish model

--- Whack A Jellyfish/ViewController.swift
+++ Whack A Jellyfish/ViewController.swift
@@ -40,15 +40,31 @@ class ViewController: UIViewController {
     
     func addNode() {
         
-        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
-        node.position = SCNVector3(0, 0, -1)
-        self.sceneView.scene.rootNode.addChildNode(node)
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyFishNode = jellyFishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        jellyFishNode?.position = SCNVector3(0, 0, -1)
+        self.sceneView.scene.rootNode.addChildNode(jellyFishNode!)
+        
+//        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+//        node.position = SCNVector3(0, 0, -1)
+//        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+//        self.sceneView.scene.rootNode.addChildNode(node)
         
     }
     
-    @objc func handleTap() {
+    @objc func handleTap(sender: UITapGestureRecognizer) {
         
-        print("tapped the sceneview")
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        
+        if hitTest.isEmpty {
+            print("didn't touch anything")
+        } else {
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry)
+        }
         
     }
     
