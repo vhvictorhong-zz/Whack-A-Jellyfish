8809efeb093f8798541cb4caf40a54137bb3de7a
victorhong1988 <victorhong1988@gmail.com>
2017-12-18 13:59:11 -0500

    Setup AR and created tap gesture

--- Whack A Jellyfish/ViewController.swift
+++ Whack A Jellyfish/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func play(_ sender: Any) {
+        
+        self.addNode()
+        
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        
+    }
+    
+    func addNode() {
+        
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.position = SCNVector3(0, 0, -1)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+    }
+    
+    @objc func handleTap() {
+        
+        print("tapped the sceneview")
+        
+    }
+    
 }
 
