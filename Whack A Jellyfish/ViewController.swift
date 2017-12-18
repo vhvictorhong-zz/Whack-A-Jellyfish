bf2a152d64d21d5a7109d34c0c7f513c243ab50f
victorhong1988 <victorhong1988@gmail.com>
2017-12-18 16:12:50 -0500

    Fix removal of jellyfish animation

--- Whack A Jellyfish/ViewController.swift
+++ Whack A Jellyfish/ViewController.swift
@@ -66,8 +66,12 @@ class ViewController: UIViewController {
             let results = hitTest.first!
             let node = results.node
             if node.animationKeys.isEmpty {
+                SCNTransaction.begin()
                 self.animateNode(node: node)
-                node.removeFromParentNode()
+                SCNTransaction.completionBlock = {
+                    node.removeFromParentNode()
+                }
+                SCNTransaction.commit()
             }
             
         }
