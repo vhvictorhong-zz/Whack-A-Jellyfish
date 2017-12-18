e5075d1fd1a9fc158a3117448ba1b7b5fc77d5c6
victorhong1988 <victorhong1988@gmail.com>
2017-12-18 13:29:03 -0500

    Initial Commit

---
+++ Whack A Jellyfish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whack A Jellyfish
+//
+//  Created by Victor Hong on 18/12/2017.
+//  Copyright © 2017 Victor Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
